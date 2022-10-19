d335bcc70c5b044813ba6b84436be496a7d44313
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2022-10-19 17:16:26 +0200

    Fix format in main.js file

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      action: 'Commander du café',
-      brand: 'Nespresso',
-      description: 'L’histoire de NESPRESSO est celle d’une passion inaltérable pour la perfection et d’une quête inlassable d’innovation en faveur de la plus haute qualité.',
-      url: 'https://www.nespresso.com/ch/fr/',
+      action: "Commander du café",
+      brand: "Nespresso",
+      description:
+        "L’histoire de NESPRESSO est celle d’une passion inaltérable pour la perfection et d’une quête inlassable d’innovation en faveur de la plus haute qualité.",
+      url: "https://www.nespresso.com/ch/fr/",
       cart: [],
       premium: true,
-    }
+    };
   },
-  methods: { 
+  methods: {
     updateCart(id) {
-      this.cart.push(id)
-    }
+      this.cart.push(id);
+    },
   },
   computed: {
     title() {
-      return this.action + ' ' + this.brand
+      return this.action + " " + this.brand;
     },
-  }
+  },
 });
